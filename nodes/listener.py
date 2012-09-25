5ab2e4ef59886e5ce9ecb58b4f3e127662ac72bb
tryexcept <xmarlawx+github@gmail.com>
2012-09-25 11:21:24 +0100

    initial commit to test out the repo and buildbot

---
+++ nodes/listener.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+import roslib
+from sensor_msgs.msg import Range
+from std_msgs.msg import Empty
+
+def cb(pub, data):
+    rospy.loginfo('{}: {}'.format(rospy.get_name(), data.range))
+    if data.range < 12:
+	pub.publish(Empty())
+
+def listener():
+    pub = rospy.Publisher('/beep', Empty)
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/ultrasound", Range, lambda x: cb(pub, x))
+    rospy.spin()
+
+if __name__ == '__main__':
+    roslib.load_manifest('arp')
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+	pass
