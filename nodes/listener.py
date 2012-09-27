e7454b3aae96030397e9ade5d6cedf9fd524bf24
tryexcept <xmarlawx+github@gmail.com>
2012-09-27 10:05:22 +0100

    test

--- nodes/listener.py
+++ nodes/listener.py
@@ -6,7 +6,7 @@ from std_msgs.msg import Empty
 
 def cb(pub, data):
     rospy.loginfo('{}: {}'.format(rospy.get_name(), data.range))
-    # need to calibrate back to CM
+    # need to calibrate back to centimeters
     if data.range < 17:
 	pub.publish(Empty())
 
