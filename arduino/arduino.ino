224d7759f46d26cb1e31adc72933dd3b35147588
tryexcept <xmarlawx+github@gmail.com>
2012-10-02 18:11:29 +0100

    a

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -123,10 +123,10 @@ void sweepAndScan()
         ++servo_pos;
     }
     myservo.write(servo_pos);
-    if (servo_pos == 50) 
+    if (servo_pos == 49) 
     {
         flag = true;
-        //ls_msg.data = &fullscan;
+        ls_msg.data = &fullscan;
         pub_laserscan.publish(&ls_msg);
     }
     else if(servo_pos == 0) {
